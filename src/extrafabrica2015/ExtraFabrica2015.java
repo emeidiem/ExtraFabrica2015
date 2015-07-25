970d341adb541cd25a65c9e35aafe9814eb668cf
emeidiem <jg.manuel@gmail.com>
2015-07-25 12:23:16 +0800

    Initial commit

---
+++ src/extrafabrica2015/ExtraFabrica2015.java
@@ -0,0 +1,17 @@
+package extrafabrica2015;
+
+import processing.core.PApplet;
+
+
+public class ExtraFabrica2015 extends PApplet {
+
+	public void setup() {
+	}
+
+	public void draw() {
+	}
+	
+	public static void main(String _args[]) {
+		PApplet.main(new String[] { extrafabrica2015.ExtraFabrica2015.class.getName() });
+	}
+}
